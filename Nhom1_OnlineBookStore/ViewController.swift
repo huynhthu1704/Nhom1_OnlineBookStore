876a6905040f87258468fafd877eacbadb05785e
huynhthu1704 <huynhthingocthu1704@gmail.com>
2022-04-12 01:16:07 -0700

    first upload

---
+++ Nhom1_OnlineBookStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nhom1_OnlineBookStore
+//
+//  Created by Huynh Thu on 2022-04-12.
+//  Copyright © 2022 Huynh Thu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nhom1_OnlineBookStoreTests/Nhom1_OnlineBookStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nhom1_OnlineBookStoreTests.swift
+//  Nhom1_OnlineBookStoreTests
+//
+//  Created by Huynh Thu on 2022-04-12.
+//  Copyright © 2022 Huynh Thu. All rights reserved.
+//
+
+import XCTest
+@testable import Nhom1_OnlineBookStore
+
+class Nhom1_OnlineBookStoreTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
